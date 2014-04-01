b99d003ccdded891ec159c179f5d74185f9a275b
vivejha <vivejha@cisco.com>
2014-04-02 03:17:43 +0530

    added all display control functions

--- nsecli.py
+++ nsecli.py
@@ -12,33 +12,113 @@ try:
     import argparse
     import logging
 except Exception, err:
-    print 'error while imports'
+    print 'error while importing module or package'
     print str(err)
+    exit()
 
-class Stock(object):
-    def __init__(self, code, mkt):
-        self.nse = mkt
-        self.code = code
-        for key, value in self.nse.get_quote(code).items():
-            self.__dict__[key] = value
 
-    def get_quote(self):
-        ''' uses market object to get the price '''
-        pass
+class NseDisplay(object):
+    ''' NseDisplay contains all the function related to displaying
+    and controlling display of results and quotes.
+    '''
+    def __init__(self, db):
+        global LOG_LEVEL
+        logging.basicConfig(level=LOG_LEVEL)
+        self.log = logging.getLogger('NseDisplay')
+        self.db = db
 
-    def refresh(self):
-        ''' refetches the stock price '''
-        pass
     def show_quote(self, quote):
-        for key, value in self.__dict__.items():
-            print key, '::', value
+        ''' controls the display of a quote '''
+        display_fields = self.db.get_config_setting('DISPLAY_FIELDS')
+        for key in display_fields:
+            if key in quote:
+                if key == 'pChange':
+                    print key, ':', quote[key], '%'
+                else:
+                    print key, ':', quote[key]
+            else:
+                print key, 'is not present in the quote'
+
+    def show_current_display_fields(self):
+        ''' shows current display fields '''
+        display_fields = self.db.get_config_setting('DISPLAY_FIELDS')
+        print 'Current display fields:'
+        for field in display_fields:
+            print field
+
+    def show_all_display_fields(self):
+        ''' shows all display fields '''
+        all_display_fields = self.db.get_config_setting('ALL_DISPLAY_FIELDS')
+        print 'All display fields:'
+        for field in all_display_fields:
+            print field
+
+    def add_display_fields(self, fields):
+        ''' adds all the display fields '''
+        self.log.debug('adding fileds %s' % fields)
+        all_display_fields = self.db.get_config_setting('ALL_DISPLAY_FIELDS')
+        current_display_fields = self.db.get_config_setting('DISPLAY_FIELDS')
+        FLAG_1 = False
+        FLAG_2 = False
+        exists = []
+        invalid = []
+
+        for field in fields:
+            if field in current_display_fields:
+                exists.append(field)
+                FLAG_1 = True
+            if field not in all_display_fields:
+                invalid.append(field)
+                FLAG_2 = True
+        if FLAG_1 is True:
+            print 'field %s already exists' % exists
+        if FLAG_2 is True:
+            print 'field %s is invalid' % invalid
+        if FLAG_1 is True or FLAG_2 is True:
+            print 'please provide valid inputs'
+            sys.exit()
+
+        # update the data base with current field
+        current_display_fields += fields
+        self.db.update_config_setting('DISPLAY_FIELDS',current_display_fields)
+
+    def remove_display_fields(self, fields):
+        ''' removed the given list of display fields '''
+        self.log.debug('removing fileds %s' % fields)
+        current_display_fields = self.db.get_config_setting('DISPLAY_FIELDS')
+        FLAG = False
+        invalid = []
+
+        for field in fields:
+            if field not in current_display_fields:
+                invalid.append(field)
+                FLAG = True
+            else:
+                current_display_fields.remove(field)
+        if FLAG is True:
+            print "field %s doesn't in current display fields" % invalid
+            print 'please provide valid inputs'
+            sys.exit()
+
+        # update the data base with current field
+        self.db.update_config_setting('DISPLAY_FIELDS', current_display_fields)
+
+    def reset_display_fields(self):
+        ''' resets all the display fields to the default one '''
+        self.log.debug('reseting DISPLAY_FIELDS setting')
+        default_display_fields = self.db.get_config_setting(\
+                                        'DEFAULT_DISPLAY_FIELDS')
+        self.db.update_config_setting('DISPLAY_FIELDS', default_display_fields)
+
 
 class NseDriver(object):
     ''' it accepts a Stock object and fetches it price
     assosiated information'''
 
     def __init__(self, db):
-        logging.basicConfig(level=logging.DEBUG)
+        # logging.basicConfig(level=logging.DEBUG)
+        global LOG_LEVEL
+        logging.basicConfig(level=LOG_LEVEL)
         self.log = logging.getLogger('NseDriver')
         self.db = db
         self.baseurl = 'http://nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?'
@@ -78,17 +158,6 @@ class NseDriver(object):
         else:
             return quote
 
-    def show_quote(self, quote):
-        ''' controls the display of a quote '''
-        display_fields = self.db.get_config_setting('DISPLAY_FILEDS')
-        for key in display_fields:
-            if key in quote:
-                if key == 'pChange':
-                    print key, ':', quote[key], '%'
-                else:
-                    print key, ':', quote[key]
-            else:
-                print key, 'is not present in the quote'
 
     def build_headers(self):
         ''' builds the headers for making http request '''
@@ -147,6 +216,8 @@ class DB(object):
     should accept this class as one of constructor requirement
     '''
     def __init__(self, db_path):
+        global LOG_LEVEL
+        logging.basicConfig(level=LOG_LEVEL)
         self.log = logging.getLogger('DB')
         self.db_path = db_path
         self.init = None
@@ -211,23 +282,59 @@ class DB(object):
             sys.exit()
         self.log.debug('config table created')
 
-        # create a row with default fileds to show
+        # create a row with default fields to show
         c = self.db.cursor()
         try:
             # TODO: SETTING field must be unique
             c.execute("INSERT INTO CONFIG (SETTING, VALUE) VALUES(\
-                      'DISPLAY_FILEDS', \
+                      'DISPLAY_FIELDS', \
+                      'lastPrice change pChange open dayHigh dayLow closePrice previousClose high52 low52')")
+            c.execute("INSERT INTO CONFIG (SETTING, VALUE) VALUES(\
+                      'DEFAULT_DISPLAY_FIELDS', \
                       'lastPrice change pChange open dayHigh dayLow closePrice previousClose high52 low52')")
         except Exception, err:
-            self.log.error('error while inserting DISPLAY_FILEDS setting')
+            self.log.error('error while inserting DISPLAY_FIELDS or DEFAULT_DISPLAY_FIELDS setting')
             self.log.error(str(err))
             self.db.rollback()
             sys.exit()
         else:
             self.db.commit()
-            self.log.debug('DISPLAY_FILEDS setting inserted successfully')
+            self.log.debug('DISPLAY_FIELDS & DEFAULT_DISPLAY_FIELDS setting inserted successfully')
 
-    def get_config_setting(self, setting):
+        # create a row with all show fields
+        all_fields = 'adhocMargin applicableMargin averagePrice bcEndDate' + ' ' + \
+            'bcStartDate buyPrice1 buyPrice2 buyPrice3' + ' ' + \
+            'buyPrice4 buyPrice5 buyQuantity1 buyQuantity2' + ' ' + \
+            'buyQuantity3 buyQuantity4 buyQuantity5 change' + ' ' + \
+            'closePrice cm_adj_high cm_adj_high_dt cm_adj_low' + ' ' + \
+            'cm_adj_low_dt cm_ffm companyName dayHigh' + ' ' + \
+            'dayLow deliveryQuantity deliveryToTradedQuantity exDate' + ' ' + \
+            'extremeLossMargin faceValue high52 indexVar' + ' ' + \
+            'isinCode lastPrice low52 marketType ' + ' ' + \
+            'ndEndDate ndStartDate open pChange' + ' ' + \
+            'previousClose priceBand pricebandlower pricebandupper' + ' ' + \
+            'purpose quantityTraded recordDate secDate' + ' ' + \
+            'securityVar sellPrice1 sellPrice2 sellPrice3' + ' ' + \
+            'sellPrice4 sellPrice5 sellQuantity1 sellQuantity2' + ' ' + \
+            'sellQuantity3 sellQuantity4 sellQuantity5 series' + ' ' + \
+            'symbol totalBuyQuantity totalSellQuantity totalTradedValue' + ' ' + \
+            'totalTradedVolume varMargin'
+        c = self.db.cursor()
+        try:
+            # TODO: SETTING field must be unique
+            c.execute("INSERT INTO CONFIG (SETTING, VALUE) VALUES(\
+                      'ALL_DISPLAY_FIELDS', \
+                      '%s')" % all_fields)
+        except Exception, err:
+            self.log.error('error while inserting ALL_DISPLAY_FIELDS setting')
+            self.log.error(str(err))
+            self.db.rollback()
+            sys.exit()
+        else:
+            self.db.commit()
+            self.log.debug('ALL_DISPLAY_FIELDS setting inserted successfully')
+
+    def get_config_setting(self, setting, ret='as_list'):
         ''' return setting as a list of strings'''
         self.log.debug('getting config setting for %s' % setting)
         c = self.db.cursor()
@@ -239,6 +346,33 @@ class DB(object):
             sys.exit()
         res = c.fetchone()[0].split()
         return [str(x) for x in res]
+
+    def update_config_setting(self, setting, value):
+        ''' updates a row with the new value in the config table '''
+        self.log.debug('updating field %s of config table' % setting)
+        self.log.debug('value is :%s' % value)
+
+        if type(value) is list:
+            value = " ".join(str(i) for i in value)
+        elif type(value) is str:
+            pass
+        else:
+            self.log.error('invalid type for %s, must be list or string'
+                           % value)
+            sys.exit()
+        c = self.db.cursor()
+        try:
+            c.execute("UPDATE CONFIG SET VALUE = '%s' WHERE SETTING = '%s'" %
+                      (value, setting))
+        except Exception, err:
+            self.log.error('error while updating %s' % setting)
+            self.log.error(str(err))
+            self.db.rollback()
+            sys.exit()
+        else:
+            self.db.commit()
+            self.log.debug('%s setting updated successfully' % setting)
+
 
 
     def get_all_stock_list(self):
@@ -261,30 +395,82 @@ class DB(object):
 ##       MAIN PROG        ##
 ############################
 
-# parse cli options
+#### PARSE CLI OPTIONS ########
 log = logging.getLogger('NseCli')
 log.info('create log')
 cparser = argparse.ArgumentParser()
-cparser.add_argument('code', action='store', help='provide the stock code')
-cparser.add_argument('-D', action="store_true", default=False,\
+cparser.add_argument('code',
+                     nargs = '?',
+                     action='store',
+                     default = False,
+                     help='provide the stock code')
+
+cparser.add_argument('-D',
+                     action="store_true",
+                     default=False,
                      help='enables debug mode for debugging purpose')
-cparser.add_argument('-reset', action="store_true", default=False,\
-                     help='resets all the settings')
-cparser.add_argument('-display_fields', action="store_true", default=False,\
-                     help='shows all the possible display fields')
+
+cparser.add_argument('-reset',
+                     action="store_true",
+                     default=False,
+                     help='resets all the display settings')
+
+cparser.add_argument('-current_display_fields',
+                     action="store_true",
+                     default=False,
+                     help='shows current display fields')
+
+cparser.add_argument('-all_display_fields',
+                     action="store_true",
+                     default=False,
+                     help='shows all possible display fields')
+
+cparser.add_argument('-add_display_fields',
+                     action="store",
+                     nargs = '*',
+                     default=False,
+                     metavar = '',
+                     help='adds a display field')
+
+cparser.add_argument('-remove_display_fields',
+                     action="store",
+                     nargs = '*',
+                     default=False,
+                     metavar = '',
+                     help='deletes a display fields')
+
 cli = cparser.parse_args()
+
+#### SET LOG LEVEL ####
 if cli.D is True:
-    logging.basicConfig(level=logging.DEBUG)
+    LOG_LEVEL = logging.DEBUG
+else:
+    LOG_LEVEL = logging.INFO
 
+#### INSTANTIATE CLASSES ####
 dirname, filename = os.path.split(os.path.abspath(__file__))
-
 db = DB(dirname + '/' + 'nse.db')
 nse = NseDriver(db)
+disp = NseDisplay(db)
 
-# initialize the database in case of first time use.
+#### INTIALYZE DB FOR THE FIRST TIME USE ####
 if db.init is False:
     db.create_stocks_table(nse.download_stock_csv())
     db.init = True
     db.create_config_table()
-nse.show_quote(nse.get_quote(cli.code))
-# TODO: return a stock object which is capable of display functions
+
+if cli.code is not False:
+    disp.show_quote(nse.get_quote(cli.code))
+else:
+    if cli.current_display_fields is True:
+        disp.show_current_display_fields()
+    elif cli.all_display_fields is True:
+        disp.show_all_display_fields()
+    elif cli.add_display_fields is not False:
+        disp.add_display_fields(cli.add_display_fields)
+    elif cli.reset is True:
+        disp.reset_display_fields()
+    elif cli.remove_display_fields is not False:
+        disp.remove_display_fields(cli.remove_display_fields)
+
+
